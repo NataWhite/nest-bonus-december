cc22a13882c9dee30e1e82c9e45f95b7c101e409
NataWhite <nata.ivanova.one@gmail.com>
2022-08-01 22:01:04 +0300

    lesson 4

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,18 +2,28 @@ import {Body, Controller, Delete, Get, Param, Patch, Post, UseGuards} from '@nes
 import {UserService} from "./user.service";
 import {CreateUserDto} from "./dto/create-user.dto";
 import {AuthGuard} from "../auth/jwt-aith.guard";
+import {CustomOkResponse} from "../core/swagger/swagger.helper";
+import {
+    SWAGGER_EXAMPLE_CREATE_USER,
+    SWAGGER_EXAMPLE_GET_ALL_USERS
+} from "../core/swagger/example/swagger-example-user.list";
 
+@ApiTag('User')
 @Controller('users')
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
     @Get()
+    @CustomOkResponse({ exampleData: SWAGGER_EXAMPLE_GET_ALL_USERS })
     @UseGuards(AuthGuard)
     getAllUsers() {
         return this.userService.getAll();
     }
 
     @Get('/:id')
+    @ApiQuery({ name: 'id', type: 'string'})
+    // @ApiQuery({ name: 'userId/', type: 'string'})
+    // @ApiQuery({ name: 'postId', type: 'string'})
     @UseGuards(AuthGuard)
     getOneUser(@Param() param: any ) {
         const { id } = param;
@@ -21,6 +31,22 @@ export class UserController {
     }
 
     @Post()
+    @ApiBody({ type: CreateUserDto})
+    @CustomOkResponse({ exampleData: SWAGGER_EXAMPLE_CREATE_USER })
+    // @ApiOkResponse({
+    //     status: 200,
+    //     schema: {
+    //         example: {
+    //             id: 1,
+    //             username: "Vadym",
+    //             email: "vadym@gmail",
+    //             age: 20,
+    //             status: false,
+    //             password: "qwerty123",
+    //             created_at: "2022-07-31T09:35:54.845Z"
+    //         }
+    //     }
+    // })
     @UseGuards(AuthGuard)
     createUser(@Body() user: CreateUserDto) {
         return this.userService.createUser(user)
